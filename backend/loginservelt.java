d56c1ce51caff216fb93d7066087e72541ffb8c7
Saumy Pandey <s8795p@gmail.com>
2022-07-20 11:38:17 +0530

    backend

---
+++ backend/loginservelt.java
@@ -0,0 +1,50 @@
+package sgsystem;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+@WebServlet("/loginuser")
+public class loginservelt extends HttpServlet  {
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String uid=req.getParameter("userid");
+		String pss=req.getParameter("pass");
+		System.out.println(uid);
+		HttpSession session=req.getSession();
+		try {
+			Connection con=sgsystemcon.connect();
+			final String sql="select * from suser where uid=? and pss=?";
+			PreparedStatement ps=con.prepareStatement(sql);
+			ps.setString(1, uid);
+			ps.setString(2,pss);
+			ResultSet rs=ps.executeQuery();
+			System.out.println(pss);
+			if(rs.next()) {
+				session.setAttribute("uid",rs.getString("uid"));
+				session.setAttribute("fname",rs.getString("fname"));
+				session.setAttribute("lname",rs.getString("lname"));
+				ps.executeUpdate();
+				con.close();
+				resp.getWriter().println("success");
+			}
+				
+			else {
+				session.setAttribute("msg","invalid password");
+				resp.sendRedirect(" ");
+			}
+				
+		}
+		catch(Exception ex) {
+			resp.getWriter().println("Error "+ex.getMessage());
+		}
+		
+	}
+}
---
+++ backend/logoutservelt.java
@@ -0,0 +1,18 @@
+package sgsystem;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+@WebServlet("/logout")
+public class logoutservelt extends HttpServlet {
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		HttpSession session=req.getSession();
+		session.invalidate();
+		resp.sendRedirect("LoginPage.jsp");
+	}
+}
